ad0a70398dab0f93a9db1c803297078adb220b75
abh7shekKr <abhi.bs.0001@gmail.com>
2024-08-15 13:15:05 +0530

    Splash Screen Added

--- app/src/main/java/com/example/nourishingfarms/HomeActivity.kt
+++ app/src/main/java/com/example/nourishingfarms/HomeActivity.kt
@@ -9,17 +9,11 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_home)
 
-        Handler(Looper.getMainLooper())
-            .postDelayed(
-                {
-                    startActivity(Intent(this, HomeActivity::class.java))
-                }, 2000
-            )
     }
 }
